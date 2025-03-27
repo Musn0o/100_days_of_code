3ae038dbc21187186995b40577bbc2d87a400753
Musn0o <askarmoustafa@yahoo.com>
2025-03-27 09:54:11 +0200

    Remove .idea/ folder from Git tracking

--- Day_68_auth/main.py
+++ Day_68_auth/main.py
@@ -18,7 +18,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
